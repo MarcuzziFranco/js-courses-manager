35b0008c8aa167ff3c171eb70d9bc81790a09103
franco <marcuzzi7franco@gmail.com>
2022-08-22 09:05:17 -0300

    incorporacion de servicios en archivo externo

---
+++ js/main.js
@@ -0,0 +1,36 @@
+import { createCourses, getCoursesList } from "./services.js";
+
+let $tittle = document.getElementById("tittle_course");
+let $vacacies = document.getElementById("vacacies_course");
+let $btn_create = document.getElementById("btn_createCourse");
+
+$btn_create.addEventListener("click", (event) => {
+  createCourses($tittle.value, $vacacies.value);
+  clearInput();
+
+  generateTableCourses();
+});
+
+function clearInput() {
+  $tittle.value = "";
+  $vacacies.value = 1;
+}
+
+function generateTableCourses() {
+  let tbody = document.getElementById("tbody");
+
+  let row = document.createElement("tr");
+  let courses = getCoursesList();
+  let count = courses.length - 1;
+
+  for (const key in courses[count]) {
+    let cell = document.createElement("td");
+    if (key !== "id") {
+      let celldata = document.createTextNode(courses[count][key]);
+      cell.appendChild(celldata);
+      row.appendChild(cell);
+    }
+  }
+
+  tbody.appendChild(row);
+}
---
+++ js/services.js
@@ -0,0 +1,53 @@
+let courses = [];
+
+//SERVICIOS
+//Obtiene todos los cursos.
+export const getCourses = () => {
+  for (let i = 0; i < courses.length; i++) {
+    const element = courses[i];
+    console.log(element);
+  }
+};
+
+export const getCoursesList = () => {
+  return courses;
+};
+
+//Obtiene un curso especifico.
+export const getCourse = (id) => {
+  return courses.find((element) => element.id === id);
+};
+
+//Permite agregar un curso a la lista.
+export const addCourses = (newCourse) => {
+  let newId = courses.length;
+  newCourse.id = newId;
+  if (courses.length > 0) newId = courses[courses.length - 1].id + 1;
+  courses.push(newCourse);
+};
+
+//Permite eliminar un curos de la lista.
+export const deleteCourse = (id) => {
+  let deleteCourse = getCourse(id);
+  courses.splice(deleteCourse.id, 1);
+};
+
+//Permite actualizar un curso.
+export const updateCourse = (course) => {
+  const indexElementUpdate = courses.findIndex(
+    (element) => element.id === course.id
+  );
+  courses[indexElementUpdate] = course;
+};
+
+// Logica ingreso Cursos
+export const createCourses = (name, vacancies) => {
+  //Creamos nuevo objeto curso.
+  let newCourse = {
+    name: name,
+    vacancies: vacancies,
+    inscriptions: 0,
+  };
+
+  addCourses(newCourse);
+};
